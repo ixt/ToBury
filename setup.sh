9432f8a17dd30a171925215ec476dc780e752f38
NfN Orange <orange@ff4500.red>
2017-02-22 00:32:56 +0000

    blind faith commit, added nginx and ssh support base install with key generation

--- setup.sh
+++ setup.sh
@@ -3,6 +3,25 @@
 
 INTERACTIVE=True
 
+get_ssh() {
+    if service ssh status | grep -q inactive; then
+        echo 1
+    else
+        echo 0
+    fi
+}
+
+enable_ssh(){
+    if [ -e /var/log/regen_ssh_keys.log ] && ! grep -q "^finished" /var/log/regen_ssh_keys.log; then
+        whiptail --msgbox "Initial ssh key generation still running. Please wait and try again." 20 60 2
+        return 1
+    fi
+    if [ $(get_ssh) -eq 0 ]; then
+        update-rc.d ssh enable &&
+        invoke-rc.d ssh start &&
+    fi
+}
+
 do_upgrade(){
     sudo apt update && sudo apt upgrade --yes
 }
@@ -31,10 +50,46 @@ do_about() {
         " 20 70 1
 }
 
+install_base() {
+    #RET=$(whiptail --yesno 
+    sudo apt update 
+    sudo apt install tor torsocks --yes
+    echo "HiddenServiceDir /var/lib/tor/toBury" >> /etc/tor/torrc
+    echo "HiddenServicePort 22 localhost:22" >> /etc/tor/torrc
+    echo "HiddenServicePort 80 localhost:80" >> /etc/tor/torrc
+    sudo service tor reload
+    HOSTNAME=$(cat /var/lib/tor/toBury/hostname)
+    sudo -u pi mkdir ~pi/.ssh -p
+    sudo -u pi ssh-keygen -t ed25519 -N "" -f "$HOSTNAME.key"
+    cat $HOSTNAME.key.pub >> ~pi/.ssh/authorized_keys
+    # This will delete the keys when you connect the first time
+    echo "sudo rm ~pi/$HOSTNAME*; sudo rm /var/www/keys.tar.gz; sudo rm ~pi/.ssh/rc" >> ~pi/.ssh/rc
+
+    sudo torsocks apt install nginx
+    rm /var/www/* -rf
+    tar cvfz /var/www/keys.tar.gz $HOSTNAME* 
+    chmod 644 /var/www/keys.tar.gz
+    cp ~pi/.tobury/configs/nginx.conf /etc/nginx/sites-enabled/default.conf
+    service nginx reload
+    
+    echo "sshd: localhost" >> /etc/hosts.allow
+    echo "ALL: ALL" >> /etc/hosts.deny
+    cp ~pi/.tobury/configs/ssh.conf /etc/ssh/sshd_config
+    enable_ssh
+    whiptail --msgbox "\
+        go to $HOSTNAME and download keys.tar.gz on your laptop \
+        \"cd ~amnesia/.ssh\" \
+        \"tar xvf ~/Tor\ Browser/keys.tar.gz\" \
+        use \"ssh pi@$HOSTNAME -i $HOSTNAME.key\" to connect \
+        after first connection you wont be able to redownload the keys so back them up \
+    " 60 20
+}
+
 if [ "$INTERACTIVE" == "True" ]; then
     while true; do
     FUN=$(whiptail --title "This One you Bury" --menu "Options" $WT_HEIGHT $WT_WIDTH $WT_MENU_HEIGHT --cancel-button Finish --ok-button Select \
     "1 Upgrade" "Updates all software on the Pi using APT" \
+    "2 Base" "Installs Tor, Enables SSH and generates keys for access (and URLs)" \
     3>&1 1>&2 2>&3)
         RET=$?
         if [ $RET -eq 1 ]; then
@@ -42,6 +97,7 @@ if [ "$INTERACTIVE" == "True" ]; then
         elif [ $RET -eq 0 ]; then
             case "$FUN" in
                 1\ *) do_upgrade;;
+                2\ *) install_base;;
                 *) whiptail --msgbox "Programming error: no option for that!" 20 60 1;;
             esac || whiptail --msgbox "There was an error running option $FUN" 20 60 1
         else 
